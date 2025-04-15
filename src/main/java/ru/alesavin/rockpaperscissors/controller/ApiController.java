9fb4c5e620f9632b069982ed965a68be1f5bcbcb
alesavin <alesavin@gmail.com>
2025-04-15 23:12:46 +0200

    commit changes; bump jquery version

--- src/main/java/ru/alesavin/rockpaperscissors/controller/ApiController.java
+++ src/main/java/ru/alesavin/rockpaperscissors/controller/ApiController.java
@@ -1,12 +1,16 @@
 package ru.alesavin.rockpaperscissors.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import ru.alesavin.rockpaperscissors.model.*;
 import ru.alesavin.rockpaperscissors.model.impl.*;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletResponse;
+import java.time.Duration;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.UUID;
@@ -27,40 +31,50 @@ public class ApiController {
     );
 
     @RequestMapping(method = RequestMethod.GET, value = "/hello")
-    public void hello(@CookieValue(name = "JSESSION", required = false) final String cookie,
-                      HttpServletResponse response) {
+    public Mono<Void> hello(@CookieValue(name = "JSESSION", required = false) final String cookie,
+                            ServerHttpResponse response) {
         if (cookie == null) {
-            Cookie newCookie = new Cookie("JSESSION", UUID.randomUUID().toString());
-            newCookie.setSecure(true);
+            ResponseCookie newCookie =
+                    ResponseCookie.from("JSESSION", UUID.randomUUID().toString())
+                            .secure(true)
+                            .build();
             response.addCookie(newCookie);
         }
+        return Mono.empty();
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "/play")
-    public Round play(@CookieValue(name = "JSESSION", required = false) final String cookie,
-                      @RequestBody final RoundRequest request) {
+    public Mono<Round> play(@CookieValue(name = "JSESSION", required = false) final String cookie,
+                            @RequestBody final RoundRequest request) {
         if (cookie == null)
             throw new SecurityException();
-        return engine.play(cookie, request);
+        return Mono.just(engine.play(cookie, request));
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "/statistics")
-    public Round[] statistics(@CookieValue(name = "JSESSION", required = false) final String cookie) {
+    @RequestMapping(method = RequestMethod.GET, value = "/statistics", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Round> statistics(@CookieValue(name = "JSESSION", required = false) final String cookie) {
         if (cookie == null)
             throw new SecurityException();
-        return engine.statistics(cookie);
+        return Flux.interval(Duration.ofSeconds(1)).map(l -> new Round(
+                new RoundRequest("a", "b" + l),
+                Shape.ROCK,
+                Shape.SCISSORS,
+                Outcome.DRAW
+        )).take(5);
+//        return Flux.fromArray(engine.statistics(cookie));
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "/play")
-    public void clear(@CookieValue(name = "JSESSION", required = false) final String cookie) {
+    public Mono<Void> clear(@CookieValue(name = "JSESSION", required = false) final String cookie) {
         if (cookie == null)
             throw new SecurityException();
         engine.clear(cookie);
+        return Mono.empty();
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/global-statistics")
-    public GlobalStatistics globalStatistics() {
-        return engine.globalStatistics();
+    public Mono<GlobalStatistics> globalStatistics() {
+        return Mono.just(engine.globalStatistics());
     }
 
     @ExceptionHandler(SecurityException.class)
